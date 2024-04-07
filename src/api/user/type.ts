cdf56f3436cc47ad105020c00e4da39aeff1533e
JP.Zhou <3214423826@qq.com>
2024-04-07 20:49:00 +0800

    chore: 管理用户相关的api

---
+++ src/api/user/type.ts
@@ -0,0 +1,37 @@
+interface dataType {
+  token: string
+}
+
+interface userInfo {
+  userId: number
+  avatar: string
+  username: string
+  password: string
+  desc: string
+  roles: string[]
+  buttons: string[]
+  routes: string[]
+  token: string
+}
+
+interface user {
+  checkUser: userInfo
+}
+
+// 登录接口需要携带参数的ts类型
+export interface loginForm {
+  username: string
+  password: string
+}
+
+// 登录接口返回的数据类型
+export interface loginResponseData {
+  code: number
+  data: dataType
+}
+
+// 获取用户信息接口返回的数据类型
+export interface userResponseData {
+  code: number
+  data: user
+}
