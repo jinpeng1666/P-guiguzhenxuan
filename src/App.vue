df790b96589162dee792e08bdf10a1cd870197ad
JP.Zhou <3214423826@qq.com>
2024-04-07 20:08:31 +0800

    test: 测试二次封装的axios

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,22 @@
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import request from '@/utils/request'
+import { onMounted } from 'vue'
+
+onMounted(() => {
+  request({
+    url: '/user/login',
+    method: 'post',
+    data: {
+      username: 'admin',
+      password: '111111',
+    },
+  }).then((res: any) => {
+    console.log(res)
+  })
+})
+</script>
 
 <style></style>
