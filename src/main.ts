174bf5574dbca2f31dae8fc6c22597baa112a203
JP.Zhou <3214423826@qq.com>
2024-04-07 20:47:14 +0800

    chore: 删除测试axios代码

--- src/main.ts
+++ src/main.ts
@@ -16,14 +16,4 @@ const app = createApp(App)
 // 安装element-plus插件
 app.use(ElementPlus)
 
-import axios from 'axios'
-axios({
-  url: '/api/user/login',
-  method: 'post',
-  data: {
-    username: 'admin',
-    password: '111111',
-  },
-})
-
 app.mount('#app')
