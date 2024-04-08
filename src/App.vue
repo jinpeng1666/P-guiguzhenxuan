023b1617d79b4b431299c875577fa7d047791b2e
JP.Zhou <3214423826@qq.com>
2024-04-08 20:11:33 +0800

    test: 测试login路由组件

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,9 @@
 <template>
   <div>
-    <h4>我是根组件</h4>
-    <el-button type="primary"></el-button>
-    <svg>
-      <use xlink:href="#icon-love"></use>
-    </svg>
     <router-view></router-view>
   </div>
 </template>
 
-<script setup lang="ts">
-import { onMounted } from 'vue'
-import { reqLogin } from './api/user'
-
-onMounted(() => {
-  reqLogin({ username: 'admin', password: '111111' })
-})
-</script>
+<script setup lang="ts"></script>
 
 <style></style>
