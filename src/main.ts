84f3e2939b3aa081fef65b8b10a52a531b573b6e
JP.Zhou <3214423826@qq.com>
2024-04-10 11:27:11 +0800

    chore: 配置pinia

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,9 @@ import 'virtual:svg-icons-register'
 // 引入路由
 import router from './router'
 
+// 引入仓库
+import pinia from './store'
+
 const app = createApp(App)
 
 // 安装element-plus插件
@@ -21,5 +24,8 @@ app.use(ElementPlus)
 
 // 注册路由
 app.use(router)
+
+// 注册仓库
+app.use(pinia)
 
 app.mount('#app')
