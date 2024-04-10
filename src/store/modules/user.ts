420899bc274eba412756dd59bfeb63526d258622
JP.Zhou <3214423826@qq.com>
2024-04-10 16:23:04 +0800

    refactor: 使用自定义的js-cookie重构token管理

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,20 +1,21 @@
 import { defineStore } from 'pinia'
+// 导入登陆api
 import { reqLogin } from '@/api/user'
 import type { loginForm } from '@/api/user/type'
+// 导入js-cookie
+import { getToken, setToken } from '@/utils/auth'
 
 const useUserStore = defineStore('User', {
   state: () => {
     return {
-      token: localStorage.getItem('TOKEN'),
+      token: getToken(),
     }
   },
   actions: {
     async userLogin(data: loginForm) {
       const result: any = await reqLogin(data)
       if (result.code == 200) {
-        this.token = result.data.token
-        // 本地持久化存储
-        localStorage.setItem('TOKEN', result.data.token)
+        setToken(result.data.token)
         return 'ok'
       } else {
         return Promise.reject(new Error(result.data.message))
