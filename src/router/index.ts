19777043208261be818c87fe5821d82a364e24f8
JP.Zhou <3214423826@qq.com>
2024-04-10 22:22:38 +0800

    feat: 新增路由守卫

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,16 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import { constantRoute } from './routes'
 
+// 引入nprogress
+import nprogress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+// 引入user仓库
+import useUserStore from '@/store/modules/user'
+
+const whiteList = ['/login', '/404']
+
+// 创建路由
 const router = createRouter({
   history: createWebHashHistory(),
   routes: constantRoute,
@@ -12,4 +22,29 @@ const router = createRouter({
   },
 })
 
+// 全局前置路由守卫
+router.beforeEach((to, from, next) => {
+  const userStore = useUserStore()
+  nprogress.start()
+  if (userStore.token) {
+    if (to.path === '/login') {
+      next('/')
+      nprogress.done()
+    } else {
+      next()
+    }
+  } else {
+    if (whiteList.includes(to.path)) {
+      next()
+    } else {
+      next('/login')
+      nprogress.done()
+    }
+  }
+})
+
+// 全局后置路由守卫
+router.afterEach(() => {
+  nprogress.done()
+})
 export default router
