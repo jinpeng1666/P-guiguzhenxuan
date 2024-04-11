6137dce533be25a5e5419ad0533df502c14baa77
JP.Zhou <3214423826@qq.com>
2024-04-11 20:34:34 +0800

    feat: 新建layout组件

---
+++ src/layout/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="layout_container">一级路由</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.layout_container {
+  width: 100%;
+  height: 100vh;
+}
+</style>
