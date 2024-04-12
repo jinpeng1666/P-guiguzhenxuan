c709b08cbb7c641885e3ec02a421868b2a5b4cd0
JP.Zhou <3214423826@qq.com>
2024-04-12 20:26:05 +0800

    feat: logo组件的搭建

---
+++ src/layout/components/logo/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="height: 30px; background-color: pink">
+    <el-row style="height: 30px">
+      <el-col :span="4" style="height: 30px">
+        <svg style="width: 30px; height: 30px">
+          <use xlink:href="#icon-logo"></use>
+        </svg>
+      </el-col>
+      <el-col class="title" :span="20" style="height: 30px">
+        {{ setting.title }}
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script setup lang="ts">
+import setting from '@/setting'
+</script>
+
+<style scoped lang="scss">
+.title {
+  font-weight: 700;
+  font-size: 25px;
+}
+</style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,16 +1,24 @@
 <template>
   <div class="common-layout">
     <el-container style="height: 100%">
-      <el-aside width="200px">左侧菜单</el-aside>
+      <!-- 左侧导航栏 -->
+      <el-aside width="200px">
+        <Logo></Logo>
+      </el-aside>
+      <!-- 右侧 -->
       <el-container style="height: 100%">
+        <!-- 右侧面包屑 -->
         <el-header height="30px">头部内容</el-header>
+        <!-- 右侧展示内容区 -->
         <el-main>主要内容</el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Logo from './components/logo/index.vue'
+</script>
 
 <style scoped lang="scss">
 .common-layout {
