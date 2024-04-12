99edc3fe1a386392e4b8b535090f21bef33907e8
JP.Zhou <3214423826@qq.com>
2024-04-12 11:44:52 +0800

    refactor: 重构layout组件静态布局

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,11 +1,19 @@
 <template>
-  <div class="layout_container">一级路由</div>
+  <div class="common-layout">
+    <el-container style="height: 100%">
+      <el-aside width="200px">左侧菜单</el-aside>
+      <el-container style="height: 100%">
+        <el-header height="30px">头部内容</el-header>
+        <el-main>主要内容</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
 <style scoped lang="scss">
-.layout_container {
+.common-layout {
   width: 100%;
   height: 100vh;
 }
