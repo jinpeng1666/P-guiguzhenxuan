51c09a8c37828f60ddb85c80d94bac35248fd8dc
JP.Zhou <3214423826@qq.com>
2024-04-15 09:54:08 +0800

    feat: 新建菜单组件

---
+++ src/layout/components/menu/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <el-scrollbar class="scrollbar">
+      <!-- element菜单 -->
+      <el-menu>
+        <el-menu-item>首页</el-menu-item>
+        <el-menu-item>盈利</el-menu-item>
+        <el-sub-menu>
+          <template #title>员工详情</template>
+          <el-menu-item>张三</el-menu-item>
+          <el-menu-item>李四</el-menu-item>
+        </el-sub-menu>
+      </el-menu>
+    </el-scrollbar>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.scrollbar {
+  width: 100%;
+  height: calc(100vh - 30px);
+}
+</style>
