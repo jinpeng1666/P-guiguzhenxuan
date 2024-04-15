1f1e112d163aadc3222af3405586da5c12172d7a
JP.Zhou <3214423826@qq.com>
2024-04-15 18:50:21 +0800

    feat: 添加新路由

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,11 +3,57 @@ export const constantRoute = [
     path: '/login',
     component: () => import('@/views/login/index.vue'),
     name: 'login',
+    meta: {
+      isShow: false,
+    },
   },
   {
     path: '/',
     component: () => import('@/layout/index.vue'),
     name: 'layout',
+    children: [
+      {
+        path: '/home',
+        component: () => import('@/views/home/index.vue'),
+        meta: {
+          title: '首页',
+        },
+        children: [],
+      },
+      {
+        path: '/employee',
+        component: () => import('@/views/employee/index.vue'),
+        meta: {
+          title: '员工',
+        },
+        children: [
+          {
+            path: '/lisi',
+            component: () => import('@/views/home/index.vue'),
+            meta: {
+              title: '李四',
+            },
+            children: [],
+          },
+          {
+            path: '/wangwu',
+            component: () => import('@/views/home/index.vue'),
+            meta: {
+              title: '王五',
+            },
+            children: [],
+          },
+        ],
+      },
+      {
+        path: '/salary',
+        component: () => import('@/views/salary/index.vue'),
+        meta: {
+          title: '薪资',
+        },
+        children: [],
+      },
+    ],
   },
   {
     path: '/404',
