1f8865d3311cba95a037ffb80bddf83ffbb510f6
JP.Zhou <3214423826@qq.com>
2024-04-15 18:52:20 +0800

    feat: 初始化

---
+++ src/views/employee/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>我是员工组件</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
---
+++ src/views/salary/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>我是薪资组件</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss"></style>
