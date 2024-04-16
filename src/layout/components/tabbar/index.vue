ad8154555bd9367aca0f4782224259187aa54fb7
JP.Zhou <3214423826@qq.com>
2024-04-16 21:59:10 +0800

    feat: 自定义tabbar组件

---
+++ src/layout/components/tabbar/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="tabbar">
+    <div class="tabbar_left">
+      <el-icon style="margin-right: 5px">
+        <Expand />
+      </el-icon>
+      <!-- 左侧面包屑 -->
+      <el-breadcrumb separator-icon="ArrowRight">
+        <el-breadcrumb-item>权限管理</el-breadcrumb-item>
+        <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      </el-breadcrumb>
+    </div>
+    <div class="tabbar_right">
+      <el-button type="primary" size="small" icon="Refresh" circle></el-button>
+      <el-button
+        type="primary"
+        size="small"
+        icon="FullScreen"
+        circle
+      ></el-button>
+      <el-button type="primary" size="small" icon="Setting" circle></el-button>
+      <img
+        src="https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif"
+        style="width: 20px; height: 20px; margin: 0 10px"
+      />
+      <!-- 下拉菜单 -->
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          admin
+          <el-icon class="el-icon--right">
+            <arrow-down />
+          </el-icon>
+        </span>
+        <template #dropdown>
+          <el-dropdown-menu>
+            <el-dropdown-item>退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.tabbar {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+
+  .tabbar_left {
+    display: flex;
+    align-items: center;
+    margin-left: 10px;
+  }
+
+  .tabbar_right {
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
