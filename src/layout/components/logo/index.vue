9066a7bc4bb529a0fa0cb1b205cf854d90c2f07d
JP.Zhou <3214423826@qq.com>
2024-04-17 11:35:45 +0800

    refactor: 高度和宽度用样式变量控制

--- src/layout/components/logo/index.vue
+++ src/layout/components/logo/index.vue
@@ -1,26 +1,51 @@
 <template>
-  <div style="height: 30px; background-color: #a3b18a">
-    <el-row style="height: 30px">
-      <el-col :span="4" style="height: 30px">
+  <div class="logo">
+    <el-row class="height" v-if="!settingStore.isCollapse">
+      <el-col :span="4" class="icon">
         <svg style="width: 30px; height: 30px">
           <use xlink:href="#icon-logo"></use>
         </svg>
       </el-col>
-      <el-col class="title" :span="20" style="height: 30px">
+      <el-col class="title" :span="20">
         {{ setting.title }}
       </el-col>
     </el-row>
+    <svg style="width: 30px; height: 30px" v-else>
+      <use xlink:href="#icon-logo"></use>
+    </svg>
   </div>
 </template>
 
 <script setup lang="ts">
 import setting from '@/setting'
+
+// 引入setting仓库
+import useSettingStore from '@/store/modules/setting.ts'
+const settingStore = useSettingStore()
 </script>
 
 <style scoped lang="scss">
+@import '@/styles/variable.scss';
+
+.logo {
+  height: $logo-height;
+  background-color: #a3b18a;
+}
+
+.icon {
+  height: $logo-height;
+  display: flex;
+  align-items: center;
+}
+
 .title {
+  height: $logo-height;
   font-weight: 700;
   font-size: 20px;
-  line-height: 30px;
+  line-height: $logo-height;
+}
+
+.height {
+  height: $logo-height;
 }
 </style>
--- src/layout/index.vue
+++ src/layout/index.vue
@@ -2,7 +2,7 @@
   <div class="common-layout">
     <el-container style="height: 100%">
       <!-- 左侧导航栏 -->
-      <el-aside width="200px" style="background-color: #a3b18a">
+      <el-aside class="aside">
         <Logo></Logo>
         <el-scrollbar class="menuScrollbar">
           <Menu :menuList="menuList"></Menu>
@@ -11,7 +11,7 @@
       <!-- 右侧 -->
       <el-container style="height: 100%">
         <!-- 右侧面包屑 -->
-        <el-header height="30px" style="background-color: #a3b18a; padding: 0">
+        <el-header class="right-header">
           <Tabbar></Tabbar>
         </el-header>
         <!-- 右侧展示内容区 -->
@@ -42,12 +42,26 @@ const menuList = computed(() => {
 </script>
 
 <style scoped lang="scss">
+@import '@/styles/variable.scss';
+
 .common-layout {
   width: 100%;
   height: 100vh;
 }
+
+.aside {
+  background-color: #a3b18a;
+  width: $el-aside-height;
+}
+
 .menuScrollbar {
   width: 100%;
-  height: calc(100vh - 30px);
+  height: calc(100vh - $logo-height);
+}
+
+.right-header {
+  height: $el-header-height;
+  background-color: #a3b18a;
+  padding: 0;
 }
 </style>
