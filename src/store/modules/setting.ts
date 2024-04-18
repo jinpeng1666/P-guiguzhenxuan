3a22c8dd4567fc28c42610f3dbcf5bb255ce868c
JP.Zhou <3214423826@qq.com>
2024-04-18 20:40:11 +0800

    feat: 新增仓库中的refsh

--- src/store/modules/setting.ts
+++ src/store/modules/setting.ts
@@ -3,7 +3,10 @@ import { defineStore } from 'pinia'
 const useSettingStore = defineStore('setting', {
   state: () => {
     return {
+      // 菜单折叠
       isCollapse: false,
+      // 刷新
+      refsh: false,
     }
   },
 })
