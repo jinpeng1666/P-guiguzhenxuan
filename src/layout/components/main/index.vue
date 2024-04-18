accc88c1aca5cf0971f7ad6cb914cff7700a9016
JP.Zhou <3214423826@qq.com>
2024-04-18 20:40:36 +0800

    feat: 新建main组件

---
+++ src/layout/components/main/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <router-view v-if="flag"></router-view>
+</template>
+
+<script setup lang="ts">
+// 引入setting仓库
+import useSettingStore from '@/store/modules/setting.ts'
+const settingStore = useSettingStore()
+
+import { watch, ref, nextTick } from 'vue'
+let flag = ref(true)
+watch(
+  () => settingStore.refsh,
+  () => {
+    flag.value = false
+    nextTick(() => {
+      flag.value = true
+    })
+  },
+)
+</script>
+<style scoped lang="scss"></style>
